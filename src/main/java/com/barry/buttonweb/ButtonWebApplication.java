70304e975c2405f3853c58ad590b2616c0d62ac7
Javaplus <btarlton@gmail.com>
2022-01-01 18:06:46 -0500

    working with tomcat

--- src/main/java/com/barry/buttonweb/ButtonWebApplication.java
+++ src/main/java/com/barry/buttonweb/ButtonWebApplication.java
@@ -2,12 +2,19 @@ package com.barry.buttonweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ButtonWebApplication {
+public class ButtonWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ButtonWebApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ButtonWebApplication.class);
+    }
 
 }
